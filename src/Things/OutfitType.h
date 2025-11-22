6d368e5cad3b4929c4adc9356cdc13d4bcd8247e
Tofame <80583148+Tofame@users.noreply.github.com>
2025-11-22 17:09:31 +0100

    Items List Indexing rework
    First ever, fully AI-done PR :)
    
    Fixes items window - fixed indexing
    Following previous fix, fixes to rendering, previews etc.
    Support for outfits,effects,missiles too
    
    Closes #6
    Closes #1 (I think)

---
+++ src/Things/OutfitType.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "ThingType.h"
+
+class OutfitType : public ThingType {
+public:
+    OutfitType();
+    virtual ~OutfitType() = default;
+    OutfitType(const OutfitType&) = default;
+    OutfitType& operator=(const OutfitType&) = default;
+};
+
---
+++ src/Things/Outfits.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <cstdint>
+#include <string>
+#include <vector>
+#include <memory>
+#include "OutfitType.h"
+
+class Outfits
+{
+public:
+    Outfits() = default;
+    virtual ~Outfits() = default;
+
+    static bool isValidOutfitTypeIndex(uint32_t id);
+    static std::shared_ptr<OutfitType> getOutfitType(uint32_t id);
+    static void pushOutfitType(std::shared_ptr<OutfitType> oType);
+    static void removeOutfitType(uint32_t id);
+    static bool replaceOutfitType(uint32_t id, std::shared_ptr<OutfitType> newOutfitType);
+    static uint32_t getOutfitTypesCount() { return static_cast<uint32_t>(outfitTypes.size()); }
+    static const std::vector<std::shared_ptr<OutfitType>>& getOutfitTypes() { return outfitTypes; }
+    static void clearOutfitTypes() { outfitTypes.clear(); }
+
+    std::shared_ptr<OutfitType> operator[](uint32_t id) const { return outfitTypes.at(id); }
+
+private:
+    static std::vector<std::shared_ptr<OutfitType>> outfitTypes;
+    static inline std::shared_ptr<OutfitType> dollOutfitType = std::make_shared<OutfitType>();
+};
+
